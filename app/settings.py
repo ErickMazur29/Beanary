831fdf1739f8a68629f75e25953039780b90de06
ErickMazur29 <mazur.negocios@gmail.com>
2026-07-25 13:46:39 -0300

    fix bugs v2

--- app/settings.py
+++ app/settings.py
@@ -133,6 +133,7 @@ STORAGES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
 
